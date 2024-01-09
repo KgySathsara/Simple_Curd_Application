aa37bb8f9c87b7254db2be7c9781730ac0d7ee54
KgySathsara <88894196+KgySathsara@users.noreply.github.com>
2024-01-09 10:43:22 +0530

    Parth Add

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ajax-crud-datatable', [EmployeeController::class, 'index']);
+Route::post('store', [EmployeeController::class, 'store']);
+Route::post('edit', [EmployeeController::class, 'edit']);
+Route::post('delete', [EmployeeController::class, 'destroy']);
